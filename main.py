c84117641ed60420d799e61034e051c51c309b62
Vilas-Gowda <vilasgowda65@gmail.com>
2022-07-30 21:28:21 +0530

    addad

--- main.py
+++ main.py
@@ -11,13 +11,12 @@ window.config(bg= "black")
 
 samplemusic = "sample music"
 pattern = "*.mp3"
-
+filename='/'
 lb = tk.Listbox(window,fg = "black",width =100 , font = ("Tahoma",15))
 lb.pack(padx=15,pady=100)
 
-lb.insert(0,"vilas")
-lb.insert(1,"is")
-lb.insert(2,"awesome")
-
+for root,dirs,files in os.walk(filename):
+    for filename in fnmatch.filter(files,pattern):
+        lb.insert('end',filename)
 
 window.mainloop()
